1d5fb26081da92ab68263344cbaa3bb7a91e6c18
vitoria pires <vitoria.pires23.03@gmail.com>
2023-07-25 10:39:55 -0300

    atividade 25 07

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.send('You are so cool');
+});
+
+router.get('/cool/beach', function(req, res, next) {
+  res.send('Adoro Praia');
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.send('Bem Vindo ao IFC');
+});
 module.exports = router;
